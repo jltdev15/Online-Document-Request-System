213efddad43aeed1853d2c53bb7138f8c38542e8
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2024-02-27 22:59:39 +0800

    update url api

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,15 +1,11 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-
-  baseURL: "http://localhost:4000/v1/api",
+  // baseURL: "http://localhost:4000/v1/api",
   // baseURL: import.meta.env.VITE_API_BASE_URL,
-  // baseURL: "https://ict-api.onrender.com/v1/api",
+  baseURL: "https://pbc-api.onrender.com/v1/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
-
 });
-
-
 
 export default axiosClient;
